989c3577f60b659e1358ca7d7c016302d4137061
sushil satyarthi <sushilsatyarthi@gmail.com>
2025-04-03 16:09:19 +0530

    vercel-json-added

--- client/src/lib/socket.ts
+++ client/src/lib/socket.ts
@@ -11,7 +11,7 @@ export function initializeSocket(): Socket | null {
     return null;
   }
   // Create and store the socket instance
-  socket = io('http://localhost:8080', {
+  socket = io('https://dev-collab-uq02.onrender.com', {
     auth: {
       token: user.token,
       user,
